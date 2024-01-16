559a22add4e03ae74505239e30c69b334e23800f
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-16 22:34:48 +0530

    Create feed.py

---
+++ feed.py
@@ -0,0 +1,54 @@
+from pyrogram import Client
+from pyrogram import errors as pyro_errors
+import time
+import secrets
+import string
+from Config import *
+
+random_string = "".join(
+  secrets.choice(string.ascii_letters + string.digits)
+  for _ in range(10)
+)
+
+
+app = Client(name=f"{random_string}", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
+
+async def forward_message():
+    msg_link = "https://t.me/sassysmm/3"
+    parts = msg_link.text.split("/")
+    channel_username = parts[3]
+    message_id = int(parts[4])
+    chat_ids = []# Replace with the actual chat IDs you want to forward the message to
+    with open("updated.txt", "r") as f:
+        content = f.read()
+        new_content = content.split("/n")
+        for i in new_content:
+            i = i.replace("https://t.me/", "@").replace(" ", "").strip()
+            chat_ids.append(i)
+    while True:
+        for i in chat_ids:
+            try:
+                await app.forward_messages(chat_id, channel_username, message_id)
+            except pyro_errors.FloodWait as e:
+                print(f"You have a floodwait of {int(e.value/60)} Minute & {int(e.value % 60)}.Please Wait Be Patience \nTill Now Group in sended : {success}\nTill Now Fail : {fail}")
+                await asyncio.sleep(int(e.value) + 100)
+            except pyro_errors.Forbidden as e:
+                await event.reply(f"Forbidden Error in `{i}`: {e}")
+                continue
+            except pyro_errors.BadRequest as e:
+                await event.reply(f"BadRequest Error in `{i}` : {e}")
+                continue
+            except Exception as e:
+                print(f"Error in sending message in {i} due to : {e}")
+                fail += 1
+                continue
+            if int(success + fail) % len(owo) == 0:
+                stime = random.randint(1200, 1500)
+                print(f"Till Now Groups in Sended :  `{success}`\nTill Now Its Fail : `{fail}`\nSleeped For : `{stime}`")
+            else:
+                stime = random.randint(2, 4)
+            time.sleep(stime)  # Add a delay of 1 second between each forward operation
+    except Exception as e:
+        print(str(e))
+
+app.run(forward_message()
